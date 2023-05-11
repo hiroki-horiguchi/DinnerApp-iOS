032a34059612ee572fcfd0537541cc17d58f1d9f
hiroki.horiguchi <hiroki.horiguchi@glpgs.com>
2023-05-11 18:00:34 +0900

    Initial Commit

---
+++ DinnerApp/DinnerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinnerAppApp.swift
+//  DinnerApp
+//
+//  Created by hiroki.horiguchi on 2023/05/11.
+//
+
+import SwiftUI
+
+@main
+struct DinnerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
